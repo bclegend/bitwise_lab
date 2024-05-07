e65d6433c9ba7dda7ea2017381c2a07267e79bba
BrianCheng-TheLegend <briancheng1229@gmail.com>
2024-05-07 13:00:06 +0800

    Add version 2 i_sqrt

--- i_sqrt.c
+++ i_sqrt.c
@@ -1,9 +1,28 @@
 // Need to use -lm to link to libm when compile
 // v1
 #include <math.h>
-int i_sqrt(int N)
+int i_sqrt_v2(int N)
 {
     int msb = (int) log2(N);
+    int a = 1 << msb;
+    int result = 0;
+    while (a != 0) {
+        if ((result + a) * (result + a) <= N)
+            result += a;
+        a >>= 1;
+    }
+    return result;
+}
+
+//v2
+int i_sqrt_v2(int N)
+{
+    int msb = 0;
+    int n = N;
+    while (n > 1) {
+        n >>= 1;
+        msb++;
+    }
     int a = 1 << msb;
     int result = 0;
     while (a != 0) {
